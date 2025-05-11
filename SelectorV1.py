678126cccb4b6e209fcde16e6ec4153dc71e3cbf
giledward <giledward1690@gmail.com>
2025-05-10 20:35:26 -0400

    Open league, cant close if the game startert bur can close client: run the cose, press shift+12, 1:open client, 2 close client, 0 kill the script. feel free to add or edit.

--- SelectorV1.py
+++ SelectorV1.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import simpledialog
 import keyboard
+import subprocess
+import os
+
 
 root = tk.Tk()
 root.withdraw()  
@@ -8,14 +11,28 @@ root.withdraw()
 
 def selectedNum(num):
     print(f"Function received number: {num}")
-    root.quit()  
+    if num == 1:
+      subprocess.Popen([r"C:\Riot Games\Riot Client\RiotClientServices.exe"], shell=True)
+    elif num == 2:
+        os.system("taskkill /f /im LeagueClient.exe")
+        os.system("taskkill /f /im League of Legends.exe")
+        os.system("taskkill /f /im RiotClientServices.exe")
+        for process in processes:
+            os.system(f'taskkill /f /im "{process}" >nul 2>&1"')
+
+    elif num == 0:
+        root.quit()  
 
 def show_input_box():
     win = tk.Toplevel(root, padx=10, pady=10)
     win.title("Quick Input")
-    win.geometry("250x100")
+    win.geometry("100x75")
+    
+    # Force focus and bring to front
+    win.lift()
     win.attributes("-topmost", True)
     win.focus_force()
+    win.after(10, lambda: win.attributes("-topmost", False))  # Reset topmost so it behaves normally after
 
     entry = tk.Entry(win)
     entry.pack(pady=10)
@@ -28,6 +45,10 @@ def show_input_box():
         except ValueError:
             print("Invalid number.")
         win.destroy()
+
+    win.bind("<Return>", lambda event: handle())
+    btn = tk.Button(win, text="OK", command=handle)
+    btn.pack()
 
     
     win.bind("<Return>", lambda event: handle())
@@ -42,3 +63,4 @@ keyboard.add_hotkey('shift+f12', trigger_popup)
 
 print("Running... Press Shift + F12 to trigger input.")
 root.mainloop()
+root.destroy()
